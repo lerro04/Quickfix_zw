e0595814e304c1be42e0d573280f3ff9b35e4a67
Elphas Masuka <masukaeliphas34@gmail.com>
2026-05-07 20:42:37 +0200

    remove uknown language

--- professional/job_board.php
+++ professional/job_board.php
@@ -15,14 +15,14 @@ $user->execute([$uid]);
 $u = $user->fetch();
 
 if($_SERVER['REQUEST_METHOD']==='POST' && isset($_POST['place_bid'])){
- if(!$u['verified']){ $msg="âŒ Your account must be verified before you can bid."; }
+ if(!$u['verified']){ $msg=" Your account must be verified before you can bid."; }
  else {
  $job_id=(int)$_POST['job_id'];
  $amount=(float)$_POST['bid_amount'];
  $message=htmlspecialchars(trim($_POST['bid_message']));
  $days=(int)$_POST['estimated_days'];
  $ex=$pdo->prepare("SELECT bid_id FROM bids WHERE job_id=? AND professional_id=?"); $ex->execute([$job_id,$uid]);
- if($ex->fetch()){ $msg="âŒ You already placed a bid on this job."; }
+ if($ex->fetch()){ $msg=" You already placed a bid on this job."; }
  else {
  $pdo->prepare("INSERT INTO bids (job_id,professional_id,bid_amount,message,estimated_days) VALUES (?,?,?,?,?)")->execute([$job_id,$uid,$amount,$message,$days]);
  $newBidId = (int)$pdo->lastInsertId();
